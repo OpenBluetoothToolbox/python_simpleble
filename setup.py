54f9ddc3a7c498931df904ad553c8a52c354f410
Kevin Dewald <5274600+kdewald@users.noreply.github.com>
2022-01-22 19:12:32 -0800

    SimplePyBLE v0.0.3 (#4)
    * Preparations for next release
    
    * Fixed bug on write_request and write_command accepting incorrect parameter types.
    
    * Removed incorrect classifier.
    
    Co-authored-by: Kevin Dewald <kevin@dewald.me>

--- setup.py
+++ setup.py
@@ -128,7 +128,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
     name="simplepyble",
-    version="0.0.2",  # ! Ensure it matches the intended release version!
+    version="0.0.3",  # ! Ensure it matches the intended release version!
     author="Kevin Dewald",
     author_email="kevin@dewald.me",
     description="The ultimate fully-fledged cross-platform BLE library, designed for simplicity and ease of use.",
@@ -138,4 +138,14 @@ setup(
     zip_safe=False,
     extras_require={},
     python_requires=">=3.6",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
 )
