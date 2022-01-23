302a82dd6d665fdd694aadf8245dc5ad55023cd8
Kevin Dewald <kevin@dewald.me>
2022-01-22 19:11:31 -0800

    Removed incorrect classifier.

--- setup.py
+++ setup.py
@@ -139,7 +139,6 @@ setup(
     extras_require={},
     python_requires=">=3.6",
     classifiers=[
-        "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
